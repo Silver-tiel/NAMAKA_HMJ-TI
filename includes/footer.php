8e2d67d27e1a8dbeb97e6aa87c540138a6d2c775
Silver-tiel <irzalamru83@gmail.com>
2026-04-22 08:09:58 +0700

    Delete includes directory

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-        <div class="footer">
-            &copy; <?php echo date("Y"); ?> HMJTI Portal - Dirancang dengan ♥ untuk Mahasiswa.
-        </div>
-    </div> <!-- end main-content -->
-</div> <!-- end app-layout -->
-</body>
-</html>
